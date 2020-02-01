098689f86871b2319f174f9f14dfec767902e113
Juan Carlos Campos <rapidclimate@gmail.com>
2020-01-31 19:35:55 -0600

    Initial commit

---
+++ app/src/main/java/com/jc/appalumno/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jc.appalumno
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+       //Invocamos al boton por medio de su id
+        ingresar.setOnClickListener {
+
+            //Aqui adentro generamos la navegacion
+            var i= Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
